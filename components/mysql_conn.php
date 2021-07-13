bdc0115a9ed0f6a299b21f2e7ea534a547c3d240
NikkeiR <urnikis.r@gmail.com>
2021-07-13 19:31:34 +0300

    initial

---
+++ components/mysql_conn.php
@@ -0,0 +1,11 @@
+<?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "secondSprintData";
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        } print("Connected succesfully" . "<br>");
+?>
---
+++ components/projects_data.php
@@ -0,0 +1,21 @@
+<?php
+        $sql = "SELECT Projects.id, Projects.name, group_concat(' ', Employees.firstname) as names FROM Projects
+        LEFT JOIN Employees ON Projects.id = Employees.project GROUP BY Projects.id";
+        $result = mysqli_query($conn, $sql);
+
+        print("<table class='table'><thead>");
+        print("<tr><th>ID</th><th>Project</th><th>Name</th></tr>");
+        print("</thead>");
+        if (mysqli_num_rows($result) > 0) { 
+            while($row = mysqli_fetch_assoc($result)) {
+                print("<tr><td>{$row["id"]}</td>
+                           <td>{$row["name"]}</td>
+                           <td>{$row["names"]}</td></tr>");
+            }
+        } else {
+            echo "0 results";
+        }
+        print("</table>");
+
+        // mysqli_close($conn);
+?>
